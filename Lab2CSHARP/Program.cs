22cc53b9e97eba8be763830842ed02876f29d1db
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-04-02 14:40:22 +0300

    Добавьте файлы проекта.

---
+++ Lab2CSHARP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите X (между -1 и 1, не включая 1): ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Результат: " + CalcLn(1 - x, n));
+    }
+
+    static double CalcLn(double x, int n)
+    {
+        double sum = 0;
+        for (int k = 1; k <= n; k++)
+        {
+            sum -= Math.Pow(x, k) / k;
+        }
+        return sum;
+    }
+}
