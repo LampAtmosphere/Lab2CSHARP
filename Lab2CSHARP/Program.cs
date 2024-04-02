ea02d3e0c20e9778d9f1375e1a359bb6a9cb19dd
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2024-04-02 16:38:49 +0300

    Исправления

--- Lab2CSHARP/Program.cs
+++ Lab2CSHARP/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main()
     {
+
         Console.Write("Введите N: ");
         int n = Convert.ToInt32(Console.ReadLine());
+   
+
 
         Console.Write("Введите X (между -1 и 1, не включая 1): ");
         double x = Convert.ToDouble(Console.ReadLine());
@@ -18,8 +22,9 @@ class Program
         double sum = 0;
         for (int k = 1; k <= n; k++)
         {
-            sum -= Math.Pow(x, k) / k;
+            sum += (k % 2 == 0 ? -1 : 1) * Math.Pow(x, k) / k;
         }
         return sum;
     }
 }
+
